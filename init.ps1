ab80aded84705c09bae7f229cc232ada77f782e6
1ardotno <einar@stenberg.cloud>
2024-12-30 15:23:29 +0100

    error handeling for environmental

--- init.ps1
+++ init.ps1
@@ -14,4 +14,15 @@ try {
 }
 
 
+#Error handeling for crendtials
+if( !$ENV:AZURE_CLIENT_ID){
+    Write-Error "AZURE_CLIENT_ID env MISSING!"
+}
+elseif( !$ENV:AZURE_CLIENT_SECRET){
+    Write-Error "AZURE_CLIENT_SECRET env MISSING!"
+}
+elseif( !$ENV:AZURE_TENANT_ID){
+    Write-Error "AZURE_TENANT_ID env MISSING!"
+}else {
     connect-wtwintuner -UseDefaultCredentials
+}
